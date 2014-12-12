c269dc61a7f39318152feb7fa4977e74739b6105
upeter <peter.urs@gmail.com>
2014-12-12 11:44:01 +0100

    working sample client->server->endpoint and back

--- src/main/scala/com/xebia/util/LatencyEchoServer.scala
+++ src/main/scala/com/xebia/util/LatencyEchoServer.scala
@@ -1,6 +1,5 @@
 package com.xebia.util
 
-import scala.io._
 import java.net.InetSocketAddress
 import akka.actor._
 import akka.actor.ActorLogging
@@ -17,7 +16,7 @@ object LatencyEchoServer extends App {
   val delay = None //Option(100)
   system.actorOf(EchoService.props(endpoint, delay), "echo-service")
 
-  StdIn.readLine(s"Hit ENTER to exit ...${System.getProperty("line.separator")}")
+  io.StdIn.readLine(s"Hit ENTER to exit ...${System.getProperty("line.separator")}")
   system.shutdown()
 }
 
@@ -30,7 +29,7 @@ class EchoService(endpoint: InetSocketAddress, delay: Option[Int]) extends Actor
   import context.system
   IO(Tcp) ! Tcp.Bind(self, endpoint)
   override def receive: Receive = {
-    case Tcp.Connected(remote, _) ⇒
+    case Tcp.Connected(remote, _) =>
       println(s"Remote address $remote connected")
       sender ! Tcp.Register(context.actorOf(EchoConnectionHandler.props(remote, sender, delay)), keepOpenOnPeerClosed = true)
   }
@@ -52,29 +51,29 @@ class EchoConnectionHandler(remote: InetSocketAddress, connection: ActorRef, del
 
   override def receive: Receive = doReceive(replyHandler)
 
-  private def doReceive(replyHandler: (String, ActorRef) ⇒ Unit): Receive = {
-    case Tcp.Received(data) ⇒
+  private def doReceive(replyHandler: (String, ActorRef) => Unit): Receive = {
+    case Tcp.Received(data) =>
       val text = data.utf8String.trim
       //println(s"Received '$text' from remote address $remote")
       text match {
-        case "close" ⇒ context.stop(self)
-        case "reset" ⇒
+        case "close" => context.stop(self)
+        case "reset" =>
           counter = 0
           sender ! Tcp.Write(ByteString(s"$counter"))
-        case _ ⇒
+        case _ =>
           counter += 1
           replyHandler(s"$counter: $text\n", sender())
       }
-    case _: Tcp.ConnectionClosed ⇒
+    case _: Tcp.ConnectionClosed =>
       println(s"Stopping, because connection for remote address $remote closed")
       context.stop(self)
-    case Terminated(`connection`) ⇒
+    case Terminated(`connection`) =>
       println(s"Stopping, because connection for remote address $remote died")
       context.stop(self)
   }
 
   //delays when delay is set
-  private def replyHandler(text: String, actorRef: ActorRef): Unit = delayCfg.map { delay ⇒
+  private def replyHandler(text: String, actorRef: ActorRef): Unit = delayCfg.map { delay =>
     context.system.scheduler.scheduleOnce(delay milliseconds, actorRef, Tcp.Write(ByteString(text)))
   } getOrElse {
     actorRef ! Tcp.Write(ByteString(text))
